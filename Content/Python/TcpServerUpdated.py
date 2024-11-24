90c96faa963625f04b1b485ca6bfcd9c44398028
Kağan <89082422+Spexso@users.noreply.github.com>
2024-11-25 02:48:52 +0300

    Consistent communication with Python Script

---
+++ Content/Python/TcpServerUpdated.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5fa3d4280c5f2155aaaa8c3a90d23d9fc28e8f90af1eea2d0a10763dc1bfc50b
+size 2310
--- Content/Python/llama3_post_request.py
+++ Content/Python/llama3_post_request.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adec1948e96b10a753fc51576235760ef904d81d4aed7adc8a8731e8962559a4
-size 623
+oid sha256:d704b8ed1dbce4563f9cb57daf8bc51a672098ab257770c10412d51cf9983380
+size 638
